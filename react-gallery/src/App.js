9d4afff1ad8116b5078c4b148899a370ef35fb57
willstutely <jonathanandrewdrake@gmail.com>
2023-01-05 14:55:44 -0600

    create config.js

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import axios from 'axios';
+import apiKey from './config';
 
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
@@ -13,7 +14,7 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("cats");
 
   const performSearch = (searchTerm) => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=2a0d8a6f0e2287f8a61b2a7c73b47611&text=${searchTerm}&tags=${searchTerm}&safe_search=1&content_type=1&format=json&nojsoncallback=1`)
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${searchTerm}&tags=${searchTerm}&safe_search=1&content_type=1&format=json&nojsoncallback=1`)
       .then(response => {
         setPhotos(response.data.photos.photo)
       })
