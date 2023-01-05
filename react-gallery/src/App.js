af692d5bfb54d51bbeae4af6d37304d1de5f0d03
willstutely <jonathanandrewdrake@gmail.com>
2023-01-05 14:43:35 -0600

    Update App.js

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
 import Gallery from './components/Gallery';
+import NotFound from './components/NotFound';
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
@@ -12,31 +13,33 @@ const App = () => {
   const [searchTerm, setSearchTerm] = useState("cats");
 
   const performSearch = (searchTerm) => {
-      axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=2a0d8a6f0e2287f8a61b2a7c73b47611&text=${searchTerm}&tags=${searchTerm}&safe_search=1&content_type=1&format=json&nojsoncallback=1`)
-        .then(response => {
-          setPhotos(response.data.photos.photo)
-          })
-        .catch(error => {
-          console.log('Error fetching and parsing data', error);
-        });
-    }
-  
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=2a0d8a6f0e2287f8a61b2a7c73b47611&text=${searchTerm}&tags=${searchTerm}&safe_search=1&content_type=1&format=json&nojsoncallback=1`)
+      .then(response => {
+        setPhotos(response.data.photos.photo)
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }
+
   useEffect(() => {
     return performSearch()
   }, [])
-  
-  return (  
+
+  return (
     <BrowserRouter>
       <div className="container" >
-        <SearchForm onSearch={performSearch} searchTerm={setSearchTerm}/>
+        <SearchForm onSearch={performSearch} searchTerm={setSearchTerm} />
         <Nav />
-        <Gallery data={photos} searchTerm={searchTerm} />
-        <Routes>
-             <Route path="/gsp" element={<Gallery data={photos} searchTerm={searchTerm} />} />
-             
-        </Routes>
         
-      
+        <Routes>
+          <Route path="/" element={<Gallery data={photos} searchTerm={searchTerm} />}/>
+          <Route path="/gsp" element={<Gallery data={photos} searchTerm={searchTerm} />} />
+          <Route path="*" element={<NotFound />} />
+
+        </Routes>
+
+
       </div>
     </BrowserRouter>
   )
